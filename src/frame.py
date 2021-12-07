078617685096578dc6bf8a95d661ffbc62ad1f7a
Kunal Samant <kunal19899@gmail.com>
2021-12-06 20:32:30 -0800

    added initial files

--- src/frame.py
+++ src/frame.py
@@ -1,3 +1,11 @@
 """
     This class creates the graph representation of the match frame and stores the nodes and the links.
 """
+
+import os
+import sys
+
+class Frame:
+
+    def __init__(self, nodes):
+        pass
---
+++ src/run.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+'''
+driver code goes here
+'''
